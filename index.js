4b0ecf2747ba060677922cd8f6b7e19f24002b27
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-05-11 19:12:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ function map(src, cb) {
     let r = []
 
     for (let i = 0; i < src.length; i++) {
-      let theElement = src[i]
-      r.push(cb(theElement))
+      let elem = src[i]
+      r.push(cb(elem)
     }
 
     return r;
   }
 
-  function reduce(src, cb, starting){
-    let r = (!!starting) ? starting : src[0]
-    let i = (!!starting) ? 0 : 1
+  function reduce(src, cb, counter){
+    let r = (!!counter) ? counter : src[0]
+    let i = (!!counter) ? 0 : 1
 
     for (; i < src.length; i++) {
       r = cb(src[i], r)
